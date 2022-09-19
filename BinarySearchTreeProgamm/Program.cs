7bacebd39f7e24729360912f0d0df0f65bb5721f
sayalipagar <pagarsayali565@gmail.com>
2022-09-19 19:29:18 +0530

    [sayali] Add:welcome to Binary Search Tree

---
+++ BinarySearchTreeProgamm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTreeProgamm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binaray Search Tree!!!")
+        }
+    }
+}
